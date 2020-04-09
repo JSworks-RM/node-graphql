e20f1aa89b46855fdaddcd3311972eb0084d3ff7
Nacho <joseignaciorm13@gmail.com>
2020-04-09 14:34:01 +0200

    Mutation type: createAuthor and updateAuthor

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from 'uuid'
 const Mutation = {
+    // ************* USER MUTATION ********************
     createUser: (parent, args, { db }, info) => {
         // If an user email exist, means that a user is already registered
         const isEmailTaken = db.users.some(user => user.email === args.email)
@@ -40,6 +41,34 @@ const Mutation = {
 
         // Mutation have to return an user type object
         return { ...userExist, ...data }
+    },
+    // ************* AUTHOR MUTATION ********************
+    createAuthor: (parent, args, { db }, info) => {
+        const author = {
+            id: uuidv4(),
+            ...args
+        }
+        db.authors.push(author)
+
+        return author
+    },
+    updateAuthor: (parent, args, { db }, info) => {
+        const { id, ...data } = args
+        const authorExist = db.authors.find(author => author.id === id)
+        if (!authorExist) {
+            throw new Error ('Author does not exist')
+        }
+
+        db.authors = db.authors.map(author => {
+            if(author.id === id) {
+                author = { ...author, ...data }
+                return author
+            }
+            return author
+        })
+
+        return { ...authorExist, ...data }
+
     }
 }
 
